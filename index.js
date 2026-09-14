a5c1d6479f32f2621b02a845962bbf2b07977106
robert krutul <robbiekrutulkrutul@gmail.com>
2026-09-14 11:10:01 -0700

    Add vault combination logic

---
+++ index.js
@@ -0,0 +1,6 @@
+code1 = "10" == 7 + 3;
+code2 = "40 " == 200 / 4 - 10;
+code3 = "39" == 39 % 50;
+message =
+  "the vault has been secured.The combination is: " + (code1 && code2 && code3);
+codea = 10 - 40 - 39();
