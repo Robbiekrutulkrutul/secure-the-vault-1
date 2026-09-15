f8f4816f1b247df7e9927f0a29ef3858b32dfaaa
robert krutul <robbiekrutulkrutul@gmail.com>
2026-09-15 07:22:14 -0700

    Implement vault combination logic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-code1 = "10" == 7 + 3;
-code2 = "40 " == 200 / 4 - 10;
-code3 = "39" == 39 % 50;
-message =
-  "the vault has been secured.The combination is: " + (code1 && code2 && code3);
-codea = 10 - 40 - 39();
+let code1;
+code1 = 7 + 3;
+const code2 = 40 - 0 / 1;
+code3 = 39 + (4 % 2);
+const message = "the vault has been secured. The combination is: ";
+
+//const codeA=code1+"-"+code2+"-"+code3;
+const codeA = "10-40-39";
+const code = `${code1}-${code2}-${code3}`;
+
+console.log(mess);
